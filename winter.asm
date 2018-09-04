8e3b708bac6409c30b32497a711d558f4923619d
aardbei <megaloler9000@gmail.com>
2018-09-03 21:12:44 -0500

    macros n stuff

--- winter.asm
+++ winter.asm
@@ -14,6 +14,94 @@ engine_mode:	.res 1
 
 .segment "CODE"
 
+;; UTILITY MACROS ;;
+
+.macro	load_bg addr
+	lda $2002
+	lda #$20
+	sta $2006
+	lda #$00
+	sta $2006
+	ldx #$0
+:
+	lda addr, x
+	sta $2007
+	inx
+	bne :-
+:
+	lda addr+$100, x
+	sta $2007
+	inx
+	bne :-
+:
+	lda addr+$200, x
+	sta $2007
+	inx
+	bne :-
+:
+	lda addr+$300, x
+	sta $2007
+	inx
+	bne :-
+.endmacro
+
+.macro	load_bg_pal addr
+	lda $2002
+	lda #$3f
+	sta $2006
+	lda #$00
+	sta $2006
+	ldx #$0
+:
+	lda addr, x
+	sta $2007
+	inx
+	cpx #$10
+	bne :-
+.endmacro
+
+.macro	vblank_wait
+:
+	bit $2002
+	bpl :-
+.endmacro
+
+.macro	enable_ppu
+	lda #$08	; enable bg
+	sta $2001
+	lda #$80	; enable nmi
+	sta $2000
+.endmacro
+
+.macro	disable_ppu
+	lda #$00
+	sta $2000	; disable nmi
+	sta $2001	; disable rendering
+.endmacro
+	
+.macro	clear_ram
+	tax		; a = 0
+:
+	sta $000, x
+	sta $100, x
+	sta $200, x
+	sta $300, x
+	sta $400, x
+	sta $500, x
+	sta $600, x
+	sta $700, x
+	inx
+	bne :-
+.endmacro
+
+.macro	set_game_state state
+	lda state
+	sta engine_mode
+.endmacro
+
+
+
+
 ;; ENGINE MODES ;;
 
 ; routines for each game mode
@@ -43,6 +131,7 @@ enter_title_screen:
 
 
 
+
 ;; INTERRUPTS ;;
 
 ; vsync
@@ -68,106 +157,20 @@ reset:
 	ldx #$ff
 	txs
 
-	; disable ppu
-	inx		; x = 0
-	stx $2000	; disable nmi
-	stx $2001	; disable rendering
-
-; wait for a vblank
-@wait:
-	bit $2002
-	bpl @wait
-
-	; clear ram
-	tax		; a = 0
-@clear_ram:
-	sta $000, x
-	sta $100, x
-	sta $200, x
-	sta $300, x
-	sta $400, x
-	sta $500, x
-	sta $600, x
-	sta $700, x
-	inx
-	bne @clear_ram
-
+	disable_ppu
+	vblank_wait
+	clear_ram
 	; TODO: init apu
-
-	; init vars
-	; set mode to enter title screen
-	lda #$00
-	sta engine_mode
-
-
-; wait for a vblank
-@wait2:
-	bit $2002
-	bpl @wait2
-
-	; init ppu
-	jsr load_bg_palette
-	jsr load_nametable
-
-	; enable ppu
-	lda #$08	; enable bg
-	sta $2001
-	lda #$80	; enable nmi
-	sta $2000
+	set_game_state #$00
+	vblank_wait
+	load_bg_pal bg_pal_00
+	load_bg nt_01
+	enable_ppu
 	
-	
-@loop:
+@forever:
 	; do nothing forever
-	jmp @loop
+	jmp @forever
 
-
-
-;; UTIL ;;
-
-load_nametable:
-	lda $2002
-	lda #$20
-	sta $2006
-	lda #$00
-	sta $2006
-	ldx #$0
-@loop:
-	lda nt_00, x
-	sta $2007
-	inx
-	bne @loop
-@loop2:
-	lda nt_00+$100, x
-	sta $2007
-	inx
-	bne @loop2
-@loop3:
-	lda nt_00+$200, x
-	sta $2007
-	inx
-	bne @loop3
-@loop4:
-	lda nt_00+$300, x
-	sta $2007
-	inx
-	bne @loop4
-	rts
-
-load_bg_palette:
-	lda $2002
-	lda #$3f
-	sta $2006
-	lda #$00
-	sta $2006
-	ldx #$0
-@loop:
-	lda bg_pal_00, x
-	sta $2007
-	inx
-	cpx #$10
-	bne @loop
-	rts
-	
 
 
 
