7965d9d6c0eab7e65e82ed223f3771ac4602240f
aardbei <megaloler9000@gmail.com>
2018-09-06 23:01:48 -0500

    workin on sprites

--- src/winter.asm
+++ src/winter.asm
@@ -10,19 +10,48 @@
 
 
 
+.struct Entity
+	identity	.byte
+	state		.byte
+	life_timer	.word
+	x_pos		.word
+	y_pos		.word
+.endstruct
+
+.struct Level
+	; width in tiles of map
+	width		.byte
+
+	; all pointers
+	palette		.word
+	map		.word
+	entities	.word
+.endstruct
+
+
+
+
 .segment "ZEROPAGE"
-engine_mode:	.res 1		; engine state
-level_pal:	.res 1		; bg palette index
-tmp:		.res 1		; tmp var???
-x_pos:		.res 1		; mirror of player x
-y_pos:		.res 1		; mirror of player y
-tile_under:	.res 1		; the tile under the player
-nt_addr:	.res 2		; address of currently loaded nametable
-tmp_addr:	.res 2		; tmp 16 bit var
-pad:		.res 1		; place to put the buttons! ^_^
+; engine state
+state:		.res 1
+
+; scratch vars
+tmp0:		.res 2
+tmp1:		.res 2
+
+; buttons
+pad:		.res 1
+pad_press:	.res 1
+
+; 12 bit camera position coordinates
+cam_x:		.res 1
+cam_y:		.res 1
+cam_high:	.res 1	; high=x, low=y
 
 .segment "BSS"
-entities:	.res $100	; entity table
+particles:	.res $100	; particle effect table
+entities:	.res $200	; entity table
+map:		.res $200	; loaded map
 
 
 
